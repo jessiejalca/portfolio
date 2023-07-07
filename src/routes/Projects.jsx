a0790ce22146f7bef01e3e0d5c8b213ab8b5817c
Jessie Jalca <jessiejalca@gmail.com>
2023-07-07 00:18:02 -0400

    feat(Projects): Add short summary

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -2,7 +2,10 @@ const Projects = () => {
   return (
     <main>
       <h2>Things I've Built.</h2>
-      <p className="sub"></p>
+      <p className="sub">
+        I've enjoyed making real things that help real people. But also some
+        stuff just for me.
+      </p>
     </main>
   )
 }
