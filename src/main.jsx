81ae274a4736ae12a73fe88ac35ef4b38dd192e7
Jessie Jalca <jessiejalca@gmail.com>
2026-04-16 00:12:42 -0400

    feat: add LangContext and wrap app in LangProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { LangProvider } from "./contexts/LangContext"
 import App from "./App"
 import Home from "./routes/Home"
 import About from "./routes/About"
@@ -11,14 +12,16 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <LangProvider>
+        <Routes>
+          <Route element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+        </LangProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
